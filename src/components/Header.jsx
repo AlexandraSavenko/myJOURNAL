745974f6f04e086bac60adf07831c2b25aaf3ccc
Sasha Savenko <sashasavenko3@gmail.com>
2025-04-09 16:30:35 +0300

    header nav styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,13 +10,17 @@ import { useState } from "react";
 export default function Header () {
         const [open, setOpen] = useState(false)
     
-    return <header className="j-header">
+    return <header>
+        <div className="container">
+            <div className="j-header">
         <DateDisplay/>
         <div className="burger">
-        <Hamburger size={18} toggled={open} toggle={setOpen}/>
+        <Hamburger size={20} toggled={open} toggle={setOpen}/>
         </div>
         <nav className={clsx('header-nav', open && 'header-nav-open')}>
         <Navigation />
         </nav>
+        </div>
+        </div>
     </header>
 }
