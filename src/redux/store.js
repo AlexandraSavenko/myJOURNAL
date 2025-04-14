8fee521df242daa4482effa00715c9b4743f8fee
Sasha Savenko <sashasavenko3@gmail.com>
2025-04-14 09:52:33 +0300

    reducer name fixed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit'
 import weatherReducer from './weatherSlice'
-import weeklyHabbitReducer from './weeklyHabitsSlice'
+import weeklyHabitReducer from './weeklyHabitsSlice'
 import dailyHabitReducer from './dailyHabitsSlice'
 export const store = configureStore({
     reducer: {
         weather: weatherReducer,
-        weeklyHabits: weeklyHabbitReducer,
+        weeklyHabits: weeklyHabitReducer,
         dailyHabits: dailyHabitReducer,
     },
 });
