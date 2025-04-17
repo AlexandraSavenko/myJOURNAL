10113dde6de73c1c4c60210d2be1ea90db6c8689
Sasha Savenko <sashasavenko3@gmail.com>
2025-04-17 11:24:16 +0300

    vars added

--- src/components/TrackerCo.jsx
+++ src/components/TrackerCo.jsx
@@ -1,53 +1,52 @@
-import css from '../scss/TrackerCo.module.scss'
-import { useSelector } from 'react-redux';
-import { dailyHabitProgress } from '../redux/dailyHabitsSlice';
-export default function TrackerCo () {
-    const myProgress = useSelector(dailyHabitProgress)
-    function getColorClass(percent) {
-        if (percent === 0) return 'progress-0';
-        if (percent <= 20) return 'progress-20';
-        if (percent <= 40) return 'progress-40';
-        if (percent <= 60) return 'progress-60';
-        if (percent <= 80) return 'progress-80';
-        return 'progress-100';
-      }
+import css from "../scss/TrackerCo.module.scss";
+import { useSelector } from "react-redux";
+import { dailyHabitProgress } from "../redux/dailyHabitsSlice";
+export default function TrackerCo() {
+  const myProgress = useSelector(dailyHabitProgress);
+  function getColorClass(percent) {
+    if (percent === 0) return "progress-0";
+    if (percent <= 20) return "progress-20";
+    if (percent <= 40) return "progress-40";
+    if (percent <= 60) return "progress-60";
+    if (percent <= 80) return "progress-80";
+    return "progress-100";
+  }
 
-    const daysInMonthList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    const now = new Date ()
-    const currentMonth = now.getMonth()
-    const currentYear = now.getFullYear()
-    const currentDaysInMonth = daysInMonthList[now.getMonth()]
-    const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay(); 
-    const adjustedFirstDay = (firstDayOfMonth + 6) % 7;
+  const daysInMonthList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  const now = new Date();
+  const currentMonth = now.getMonth();
+  const currentYear = now.getFullYear();
+  const currentDaysInMonth = daysInMonthList[now.getMonth()];
+  const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+  const adjustedFirstDay = (firstDayOfMonth + 6) % 7;
 
-    const calendarArr = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-    for (let i = 0; i < adjustedFirstDay; i++) {
-        calendarArr.push(null);
-      }
-    for (let i = 1; i<= currentDaysInMonth; i++){
-        calendarArr.push(i)
-    }
-   
-    return <div className={css.tracker}>
-<p>Current month</p>
-<div className={css.days}>
-{calendarArr.map((day, index) => {
-
-const dateKey = day || null;
-
-  const percent = myProgress[dateKey] || 0;
-  const colorClass = getColorClass(percent);
+  const calendarArr = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  for (let i = 0; i < adjustedFirstDay; i++) {
+    calendarArr.push(null);
+  }
+  for (let i = 1; i <= currentDaysInMonth; i++) {
+    calendarArr.push(i);
+  }
 
   return (
-    <div
-      key={index}
-      className={`${css.day} ${day ? css[colorClass] : css.empty}`}
-    >
-      {day}
+    <div className={css.tracker}>
+      <p>Current month</p>
+      <div className={css.days}>
+        {calendarArr.map((day, index) => {
+          const dateKey = day || null;
+
+          const percent = myProgress[dateKey] || 0;
+          const colorClass = getColorClass(percent);
+
+          return (
+            <div
+              key={index}
+              className={`${css.day} ${day ? css[colorClass] : css.empty}`}>
+              {day}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-})}
-</div>
-    </div>
-    
 }
--- src/pages/Mood.jsx
+++ src/pages/Mood.jsx
@@ -1,10 +1,11 @@
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import MoodForm from "../components/MoodForm";
-import { moodTrackerList } from "../redux/moodSlice";
+// import { moodTrackerList } from "../redux/moodSlice";
 
 export function Mood (){
-    const myMood = useSelector(moodTrackerList)
-    console.log(myMood)
+    // const myMood = useSelector(moodTrackerList)
+
+    
 
     return <div>
         <MoodForm/>
