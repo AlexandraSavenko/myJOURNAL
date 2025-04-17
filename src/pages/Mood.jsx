0c9ffd1a28929332c9862aa59666db5fa98065f8
Sasha Savenko <sashasavenko3@gmail.com>
2025-04-17 13:19:58 +0300

    tracker added to mood

--- src/pages/Mood.jsx
+++ src/pages/Mood.jsx
@@ -1,16 +1,25 @@
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import MoodForm from "../components/MoodForm";
-// import { moodTrackerList } from "../redux/moodSlice";
+import TrackerCo from "../components/TrackerCo";
+import { moodTrackerList } from "../redux/moodSlice";
 import css from '../scss/Mood.module.scss';
 
 export function Mood (){
-    // const myMood = useSelector(moodTrackerList)
+    const myMood = useSelector(moodTrackerList)
 
+    function getColorClass(percent) {
+        if (percent === 0) return "progress-0";
+        if (percent <= 20) return "progress-20";
+        if (percent <= 40) return "progress-40";
+        if (percent <= 60) return "progress-60";
+        if (percent <= 80) return "progress-80";
+        return "progress-100";
+      }
     
 
     return <div>
         <MoodForm/>
     <h1 className={css.header}>Mood</h1>
-    
+    <TrackerCo getColorClass={getColorClass} progressData={myMood}/>
     </div>
 }
