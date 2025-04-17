76659524629dd3a622cdd51b96d9938bc63c006c
Sasha Savenko <sashasavenko3@gmail.com>
2025-04-17 15:16:56 +0300

    weekly progress saves

--- src/pages/Weekly.jsx
+++ src/pages/Weekly.jsx
@@ -1,14 +1,18 @@
 import { useSelector } from 'react-redux'
 import HabitForm from '../components/HabitForm'
-import { weeklyHabitsArr } from '../redux/weeklyHabitsSlice'
+import { weeklyHabitProgress, weeklyHabitsArr } from '../redux/weeklyHabitsSlice'
 import HabitElement from '../components/HabitElement'
 import { useDispatch } from "react-redux"
-import { addNewHabit, addCount, lowCount, deleteHabit } from "../redux/weeklyHabitsSlice"
+import { addNewHabit, addCount, lowCount, deleteHabit, saveWeeklyProgress } from "../redux/weeklyHabitsSlice"
 import {nanoid} from '@reduxjs/toolkit';
+import { useEffect } from 'react'
 
 export function Weekly () {  
     const dispatch = useDispatch() 
     const myHabits = useSelector(weeklyHabitsArr)
+    const myProgress = useSelector(weeklyHabitProgress)
+    console.log(myProgress)
+    const totalForWeek = myHabits.reduce((acc, el) => el.repeat === 'Once' ? acc += 1 : acc += Number(el.repeatTimes), 0)
     const totalDone = myHabits.reduce((acc, el) => acc += el.count, 0)
     const handleNewHabit = (formValue) => {
         dispatch(addNewHabit({id: nanoid(), ...formValue, count: 0}))
@@ -22,6 +26,14 @@ export function Weekly () {
    const handleDelete = (id) => {
     dispatch(deleteHabit(id))
    }
+
+   useEffect(() => {
+    const day = new Date().getDay();
+    const thisWeek = Object.keys(myProgress).length > 0 ? Object.keys(myProgress)[Object.keys(myProgress).length - 1] : 1;
+    const weekId = day !== 1 ? thisWeek : thisWeek + 1;
+       const progress = Math.round((totalDone / totalForWeek) * 100);
+       dispatch(saveWeeklyProgress({ weekId: weekId, progress }));
+       }, [totalDone, totalForWeek, dispatch]);
 
     return <section id="weekly"> 
     <HabitForm onSubmit={handleNewHabit}/>    
--- src/redux/weeklyHabitsSlice.js
+++ src/redux/weeklyHabitsSlice.js
@@ -1,10 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const weeklyHabitsArr = (state) => state.weeklyHabits.weeklyHabitsList
+export const weeklyHabitsArr = (state) => state.weeklyHabits.weeklyHabitsList;
+export const weeklyHabitProgress = (state) => state.weeklyHabits.weeklyProgress;
+
 export const slice = createSlice({
   name: "weeklyHabits",
   initialState: {
     weeklyHabitsList: [],
+    weeklyProgress: {},
     // isLoading: false,
     // error: false,
   },
@@ -29,10 +32,18 @@ export const slice = createSlice({
   },
   deleteHabit: (state, action) => {
     state.weeklyHabitsList = state.weeklyHabitsList.filter(habit => habit.id !== action.payload)
+  },
+  saveWeeklyProgress: (state, action) => {
+    const {weekId, progress} = action.payload;   
+    if (weekId !== undefined && progress !== undefined) {
+      state.weeklyProgress[weekId] = progress;
+    } else {
+      console.warn("⚠️ Missing 'week id' or 'progress' in payload:", action.payload);
+    }
   }
   }
 });
 
 
-export const {addNewHabit, addCount, lowCount, deleteHabit} = slice.actions;
+export const {addNewHabit, addCount, lowCount, deleteHabit, saveWeeklyProgress} = slice.actions;
 export default slice.reducer;
